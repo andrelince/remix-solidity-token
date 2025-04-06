6a93139951535ecac8612b057df5f00eb9615079
andrelince <alince.dev@gmail.com>
2025-04-07 00:18:07 +0100

    feat: contract with flattened

--- influent.sol
+++ influent.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.22;
 import "@openzeppelin/contracts@5.2.0/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@5.2.0/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts@5.2.0/access/Ownable.sol";
+import {ERC20Permit} from "@openzeppelin/contracts@5.2.0/token/ERC20/extensions/ERC20Permit.sol";
 
-contract Influent is ERC20, ERC20Burnable, Ownable {
+contract Influent is ERC20, ERC20Burnable, Ownable, ERC20Permit {
     uint256 public constant MAX_SUPPLY = 1_000_000 * 10 ** 18;
 
     constructor(address initialOwner)
         ERC20("Influent", "INF")
+        ERC20Permit("Influent")
         Ownable(initialOwner)
     {}
 
